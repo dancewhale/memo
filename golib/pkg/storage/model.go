51264053908e10781df08e72c432b781e46b5a8d
dancewhale <542727233@qq.com>
2024-06-12 18:15:08 +0800

    Dev: 简化了数据结构,把fsrs 结构直接内嵌gorm 中
    把gorm 数据结构作为基本结构,减少中间传导导致的问题.

--- golib/pkg/storage/model.go
+++ golib/pkg/storage/model.go
@@ -3,7 +3,6 @@ package storage
 import (
 	fsrs "github.com/open-spaced-repetition/go-fsrs"
 	"gorm.io/gorm"
-	"time"
 )
 
 type Note struct {
@@ -18,25 +17,13 @@ type Note struct {
 
 type Card struct {
 	gorm.Model
-	Due           time.Time     `json:"Due"`
-	Stability     float64       `json:"Stability"`
-	Difficulty    float64       `json:"Difficulty"`
-	ElapsedDays   uint64        `json:"ElapsedDays"`
-	ScheduledDays uint64        `json:"ScheduledDays"`
-	Reps          uint64        `json:"Reps"`
-	Lapses        uint64        `json:"Lapses"`
-	State         fsrs.State    `json:"State"`
-	LastReview    time.Time     `json:"LastReview"`
-	NoteID	      uint          `json:"NoteID"`
+	fcard         fsrs.Card    `gorm:"embedded"`
+	NoteID	      uint         `json:"NoteID"`
 }
 
 type ReviewLog struct {
 	gorm.Model
-	Rating        fsrs.Rating    `json:"Rating"`
-	ScheduledDays uint64    `json:"ScheduledDays"`
-	ElapsedDays   uint64    `json:"ElapsedDays"`
-	Review        time.Time `json:"Review"`
-	State         fsrs.State     `json:"State"`
+	flog	  fsrs.ReviewLog `gorm:"embedded"`
 	NoteID	      uint          `json:"NoteID"`
 }
 
