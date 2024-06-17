6b4444eecb90c299d6e6db3bf78d1163f3aa90a8
caoyuanzhi <542727233@qq.com>
2024-06-17 20:25:46 +0800

    Feat: 开始编写基础el 代码，创建note
    感谢anki-editor 包的编写者，大量代码借鉴于他的包。

--- memo.el
+++ memo.el
@@ -1,6 +1,119 @@
-(setq load-path (append load-path (list (file-truename "~/cao/memo/golib"))))
+;;; memo.el --- Use for memo (interactive "P") -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2024 whale
+;;
+;; Author: whale <whale@MacBook-Pro-4.local>
+;; Maintainer: whale <whale@MacBook-Pro-4.local>
+;; Created: 6月 17, 2024
+;; Modified: 6月 17, 2024
+;; Version: 0.0.1
+;; Keywords: memo space repetition
+;; Homepage: https://github.com/dancewhale/memo
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;  Thanks  Author of anki-editor, many code borrow from his package.
+;;  Use for memo (interactive "P")
+;;
+;;; Code:
 
-(load-file "~/cao/memo/golib/memo.so")
-(setq testj (memo-create-card "font test" "back test"))
+(require 'cl-lib)
+(require 'org-element)
+
+					;(setq load-path (append load-path (list (file-truename "~/cao/memo/golib"))))
+
+					;(load-file "~/cao/memo/golib/memo.so")
+					;(setq testj (memo-create-card "font test" "back test"))
+
+;;; Core primitives
+
+(defconst memo-prop-note-type "MEMO_NOTE_TYPE")
+(defconst memo-prop-note-id   "ID")
+
+(cl-defstruct memo-note
+  id type content)
 
 
+(defun memo-push-note-at-point ()
+  "Push note at point to memo db.
+If heading without an `MEMO_NOTE_TYPE' property push failed.
+If heading without an `ID' property create it."
+  (interactive)
+  (save-excursion
+    (let ((note-type) (note-id))
+      (setq note-type
+	    (org-entry-get nil memo-prop-note-type))
+      (setq note-id (org-id-get-create))
+      (if (not note-type)
+	  (user-error "No note to push found")
+	(memo--push-note (memo-note-at-point))
+	(message "Successfully pushed note at point to Anki.")))))
+
+
+(defun memo-note-at-point ()
+  "Make a note struct from current entry."
+  (let* ((note-id (org-id-get-create))
+	 (note-type (org-entry-get nil memo-prop-note-type))
+	 (content-current-heading
+	  (memo--note-contents-current-heading)))
+    (unless note-type (user-error "Missing note type"))
+    (make-memo-note :id note-id
+		    :type note-type
+		    :content content-current-heading)))
+
+
+(defun memo--push-note (note)
+  "Request AnkiConnect for updating or creating NOTE."
+  (cond
+   ((null (memo-get-note-from-id note))
+    (memo--create-note note))
+   (t
+    (memo--update-note note))))
+
+
+
+(defun memo--note-contents-current-heading ()
+  "Get content between heading at point and next sub/heading.
+
+Leading whitespace, drawers, and planning content is skipped."
+  (save-excursion
+    (let* ((element (org-element-at-point))
+	   (begin (cl-loop for eoh = (org-element-property
+				      :contents-begin element)
+			   then (org-element-property :end subelem)
+			   while eoh
+			   for subelem = (progn
+					   (goto-char eoh)
+					   (org-element-context))
+			   while (memq (org-element-type subelem)
+				       '(drawer planning property-drawer))
+			   finally return (and eoh (org-element-property
+						    :begin subelem))))
+	   (end (cl-loop for eoh = (org-element-property
+				    :contents-begin element)
+			 then (org-element-property :end nextelem)
+			 while eoh
+			 for nextelem = (progn
+					  (goto-char eoh)
+					  (org-element-at-point))
+			 while (not (or (memq (org-element-type nextelem)
+					      '(headline))
+					(eobp)))
+			 finally return (and eoh (if (eobp)
+						     (org-element-property :end nextelem)
+						   (org-element-property :begin nextelem)))))
+	   (contents-raw (or (and begin
+				  end
+				  (buffer-substring-no-properties
+				   begin
+				   ;; in case the buffer is narrowed,
+				   ;; e.g. by `org-map-entries' when
+				   ;; scope is `tree'
+				   (min (point-max) end)))
+			     "")))
+      contents-raw)))
+
+(provide 'memo)
+;;; memo.el ends here
---
+++ test/tmp.el
@@ -0,0 +1,24 @@
+;; 测试研究使用的代码
+(defun memo-update-create-note ()
+  (interactive)
+  (setq memo--property (memo--get-property-id))
+  (message memo--ank)
+  )
+
+
+(defun memo--get-property-id ()
+  (->> (org-ml-parse-headline-at (point))
+       (org-ml-headline-get-node-property "ID")))
+
+
+(defun memo--get-content ()
+  (interactive)
+  (->> (org-ml-parse-section-at (point))
+       (org-ml-to-trimmed-string)
+       )
+  )
+
+(defun memo-get-content ()
+  (->> (org-ml-parse-this-headline)
+       (org-ml-headline-get-contents (list :log-into-drawer t :clock-into-drawer t :clock-out-notes t))
+       (org-ml-to-trimmed-string)))
